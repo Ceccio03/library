13dc242b9c26040dd880913b010a25a8f4615096
Ceccio03 <76108781+Ceccio03@users.noreply.github.com>
2023-07-19 09:04:24 +0200

    Update main.js

--- main.js
+++ main.js
@@ -11,27 +11,25 @@ function render(){
     const booksContainer = document.getElementById('main-container');
     booksContainer.innerHTML = '';
 
-    if (library){
+    if (library) {
+        for (let i = 0; i < library.bookArray.length; i++) {
+            const book = library.bookArray[i];
+            const div = document.createElement('div');
+            div.classList.add('book-card');
 
-    for (let i = 0; i < library.bookArray.length; i++) {
-        const book = library.bookArray[i];
-        const div = document.createElement('div');
-        div.classList.add('book-card');
+            const titleStrong = document.createElement('strong');
+            const titleNode = document.createTextNode(book.title);
+            titleStrong.appendChild(titleNode);
+            div.appendChild(titleStrong);
 
-        const titleStrong = document.createElement('strong');
-        const titleNode = document.createTextNode(book.title);
-        titleStrong.appendChild(titleNode);
-        div.appendChild(titleStrong);
+            const authorSpan = document.createElement('span');
+            const authorNode = document.createTextNode(book.author);
+            authorSpan.appendChild(authorNode);
+            div.appendChild(authorSpan);
 
-        const authorSpan = document.createElement('span');
-        const authorNode = document.createTextNode(book.author);
-        authorSpan.appendChild(authorNode);
-        div.appendChild(authorSpan);
-
-        booksContainer.appendChild(div);
-        
+            booksContainer.appendChild(div);
+            
         }
-      
     }
 }
 render();
